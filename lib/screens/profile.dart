3a639116a1a9b27fea0ab7cc7f00bc471f481d64
TranHoaiiTrong-CDTH19PMC <0306191286@caothang,edu.vn>
2022-01-22 23:20:49 +0700

    timkiem_laydsvetrangchu_themxoasua_xemdiadanhmoi

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,47 +1,47 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class Profile extends StatefulWidget {
-  @override
-  ProfileScreen createState() => ProfileScreen();
-}
+// class Profile extends StatefulWidget {
+//   @override
+//   ProfileScreen createState() => ProfileScreen();
+// }
 
-class ProfileScreen extends State<Profile> {
-  static const routeName = '/profile';
-  @override
-  Widget build(BuildContext context) {
-    Widget hang1 = Container(
-      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ListTile(
-            leading: Icon(Icons.people),
-            title: Text("Nguyễn Anh Khoa"),
-            trailing: Icon(Icons.chevron_right),
-          ),
-          ListTile(
-            leading: Icon(Icons.email),
-            title: Text("nguyenanhkhoa@gmail.com"),
-            trailing: Icon(Icons.chevron_right),
-          ),
-          ListTile(
-            leading: Icon(Icons.phone),
-            title: Text('0306191234'),
-            trailing: Icon(Icons.chevron_right),
-          )
-        ],
-      ),
-    );
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Account'),
-          backgroundColor: Colors.indigo,
-        ),
-        body: Column(
-          children: [
-            hang1,
-            Center(child: Column(children: [])),
-          ],
-        ));
-  }
-}
+// class ProfileScreen extends State<Profile> {
+//   static const routeName = '/profile';
+//   @override
+//   Widget build(BuildContext context) {
+//     Widget hang1 = Container(
+//       padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           ListTile(
+//             leading: Icon(Icons.people),
+//             title: Text("Nguyễn Anh Khoa"),
+//             trailing: Icon(Icons.chevron_right),
+//           ),
+//           ListTile(
+//             leading: Icon(Icons.email),
+//             title: Text("nguyenanhkhoa@gmail.com"),
+//             trailing: Icon(Icons.chevron_right),
+//           ),
+//           ListTile(
+//             leading: Icon(Icons.phone),
+//             title: Text('0306191234'),
+//             trailing: Icon(Icons.chevron_right),
+//           )
+//         ],
+//       ),
+//     );
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Account'),
+//           backgroundColor: Colors.indigo,
+//         ),
+//         body: Column(
+//           children: [
+//             hang1,
+//             Center(child: Column(children: [])),
+//           ],
+//         ));
+//   }
+// }
